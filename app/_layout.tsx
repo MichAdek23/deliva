ed08f7f11bfd389626be13bc7bcda7cdde42d47c
Adeleke Michael Olamide <adelekem600@gmail.com>
2025-12-03 09:42:56 +0100

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,9 +7,9 @@ export default function RootLayout() {
 
   return (
     <>
-      <Stack screenOptions={{ headerShown: false }} initialRouteName="splash">
-        <Stack.Screen name="onboarding" />
+      <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="splash" />
+        <Stack.Screen name="onboarding" />
         <Stack.Screen name="auth" />
         <Stack.Screen name="(consumer)" />
         <Stack.Screen name="(vendor)" />
